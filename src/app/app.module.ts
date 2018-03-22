280ae06a6e7e9bdfcd846eec1bdba2e390f40658
Pavlo Kuchereshko <atos.ua@gmail.com>
2018-03-22 21:56:29 +0200

    osm
    osm
    
    osm
    
    osm
    
    osm
    
    osm
    
    osm
    
    osm
    
    osm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { AppComponent } from "./app.component";
+import { NavigatorComponent } from "./navigator/navigator.component";
+import { MapComponent } from "./map/map.component";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+
+import { MapService } from "./map.service";
+import { GeocodingService } from "./geocoding.service";
+
+import { HeaderComponent } from "./header/header.component";
+
+import "leaflet";
+import "leaflet.vectorgrid";
+import {FooterComponent} from "./footer/footer.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @NgModule({
   declarations: [
+    NavigatorComponent,
+    MapComponent,
     AppComponent,
+    ToolbarComponent,
     NavbarComponent,
     HeaderComponent,
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatSnackBarModule
   ],
-  providers: [],
-  bootstrap: [AppComponent, HeaderComponent]
+  providers: [MapService, GeocodingService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,65 @@
+import {Component, OnInit} from "@angular/core";
+import {Observable} from "rxjs/Rx";
+import * as L from "leaflet";
+import {GeocodingService} from "../geocoding.service";
+import {MapService} from "../map.service";
+import {Location} from "../location";
+
+import "rxjs/add/operator/catch";
+
+@Component({
+  selector: "app-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.scss"]
+})
+export class MapComponent implements OnInit {
+  address: string;
+
+  constructor(private mapService: MapService,
+              private geocoder: GeocodingService) {
+    this.address = "";
+  }
+
+  ngOnInit() {
+    this.geocoder
+      .getClientLocation()
+      .catch(err => {
+        console.error(err);
+
+        // default map location
+        const location = new Location();
+        location.address = "New York City, New York, United States";
+        location.latlng = L.latLng(40.731253, -73.996139);
+
+        return Observable.of(location);
+      })
+      .subscribe((location: Location) => {
+        const map = L.map("map", {
+          zoomControl: false,
+          center: location.latlng,
+          zoom: 12,
+          minZoom: 4,
+          maxZoom: 19,
+          layers: [this.mapService.baseMaps.OpenStreetMap]
+        });
+
+        L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a>",
+          subdomains: ["a", "b", "c"]
+        }).addTo(map);
+
+        // L.tileLayer("http://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png", {
+        //     minZoom: 2,
+        //     maxZoom: 19,
+        //     tileSize: 256
+        //   }).addTo(map);
+
+        L.control.zoom({position: "topright"}).addTo(map);
+        L.control.layers(this.mapService.baseMaps).addTo(map);
+        L.control.scale().addTo(map);
+
+        this.address = location.address;
+        this.mapService.map = map;
+      });
+  }
+}
